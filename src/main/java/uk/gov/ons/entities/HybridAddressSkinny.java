ddb48a24243839aedd52b870715a4d9cb0c08057
gsmanderson <gsmanderson@gmail.com>
2021-01-22 15:08:39 +0000

    fwmt-3191 - wrapped lpi in a list and added ENG language

--- src/main/java/uk/gov/ons/entities/HybridAddressSkinny.java
+++ src/main/java/uk/gov/ons/entities/HybridAddressSkinny.java
@@ -7,14 +7,16 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import uk.gov.ons.util.CreateAddressConstants.CountryCode;
 
+import java.util.List;
+
 @Document(indexName = "#{@config.skinnyIndexName}", createIndex = false)
 @TypeAlias("unit-address-skinny")
 @EqualsAndHashCode(callSuper = true)
 public @Data class HybridAddressSkinny extends HybridAddress {
 
-	private LpiSkinny lpi;
+	private final List<LpiSkinny> lpi;
 
-	public HybridAddressSkinny(Long uprn, LpiSkinny lpi, String classificationCode, String censusAddressType, String censusEstabType,
+	public HybridAddressSkinny(Long uprn, List<LpiSkinny> lpi, String classificationCode, String censusAddressType, String censusEstabType,
 			String postcode, CountryCode countryCode, String postTown) {
 		super(uprn, classificationCode, censusAddressType, censusEstabType, postcode, countryCode, postTown);
 		this.lpi = lpi;
